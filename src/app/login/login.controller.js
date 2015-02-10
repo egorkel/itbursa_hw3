d6a80f93414ba55f3401ced8fbd04250152abdb1
Yegor Kolesnikov <kelegorkel@gmail.com>
2015-02-10 21:12:35 +0200

    added page1 status and correct login.html

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -1,20 +1,25 @@
 'use strict';
 
 angular.module('hw3', ['ui.router'])
-  .controller('loginCtrl', ['$scope', '$window',
-    function ($scope, $window) {
-      $window.localStorage.name = 'test';
-      console.log($window.localStorage.name);
+  .controller('loginCtrl', ['$scope', '$window', '$state',
+    function ($scope, $window, $state) {
+      $window.localStorage.name = '';
+
+      $scope.login = function (userName) {
+        $window.localStorage.name = userName;
+        $state.go('page1');
+      };
     }])
+
   .config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
       $stateProvider
-        .state('login', {
+        .state('login',
+        {
           url: '/login',
           templateUrl: 'app/login/login.html',
           controller: 'loginCtrl'
         });
 
       $urlRouterProvider.otherwise('/login');
-    }])
-;
+    }]);
--- src/app/page1/page1.controller.js
+++ src/app/page1/page1.controller.js
@@ -3,4 +3,15 @@
 angular.module('hw3')
   .controller('page1Ctrl', function () {
 
-  });
+  })
+
+  .config(['$stateProvider',
+    function ($stateProvider) {
+      $stateProvider
+        .state('page1',
+        {
+          url: '/page1',
+          templateUrl: 'app/page1/page1.html',
+          controller: 'page1Ctrl'
+        });
+    }]);
